5a303de09b8f484b44c03dd414a59fc407fb888a
Treza Peter <trezapeter89@gmail.com>
2020-07-20 00:52:51 +0530

    added error

--- src/lambda/hello_async.js
+++ src/lambda/hello_async.js
@@ -3,9 +3,10 @@ import cron from 'node-cron';
 exports.handler = async (event, context) => {
 
   cron.schedule('* * * * *', () => {
-    console.log('running a task every minute');
+    console.error('running a task every minute');
     //getLatestNav();
   });
+  console.error("testing console")
   return {
     statusCode: 200,
     body: "Hello, World "

55f60577ea4afee14b09bcac783dbeb74bf6364c
Treza Peter <trezapeter89@gmail.com>
2020-07-20 01:24:30 +0530

    added mongo insert

--- src/lambda/hello_async.js
+++ src/lambda/hello_async.js
@@ -1,9 +1,19 @@
 import cron from 'node-cron';
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
 exports.handler = async (event, context) => {
 
+  var thingSchema = new Schema({}, { strict: false });
+  var Thing = mongoose.model('test', thingSchema);
+
+  const connectionUrl = 'mongodb+srv://test:test@nps.uc8zf.mongodb.net/test?retryWrites=true&w=majority';
+  await mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
   cron.schedule('* * * * *', () => {
     console.error('running a task every minute');
+    var thing = new Thing({ iAmNotInTheSchema: true });
+    thing.save()
     //getLatestNav();
   });
   console.error("testing console")

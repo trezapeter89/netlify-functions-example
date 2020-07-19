e017b5f23ae0dd38cf0d9fce5836be9afad02826
Treza Peter <trezapeter89@gmail.com>
2020-07-20 00:21:17 +0530

    for ever second cron fix timzone

--- src/lambda/hello_async.js
+++ src/lambda/hello_async.js
@@ -2,10 +2,10 @@ import cron from 'node-cron';
 
 exports.handler = async (event, context) => {
 
-  cron.schedule('* * * * *', { timezone: "Asia/Kolkata" }, () => {
+  cron.schedule('* * * * *', () => {
     console.log('running a task every minute');
     //getLatestNav();
-  });
+  }, { timezone: "Asia/Kolkata" });
   return {
     statusCode: 200,
     body: "Hello, World "

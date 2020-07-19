04d0be6d73e6aea367dab923049af9a396265ea8
Treza Peter <trezapeter89@gmail.com>
2020-07-20 00:45:02 +0530

    removed timezone

--- src/lambda/hello_async.js
+++ src/lambda/hello_async.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   cron.schedule('* * * * *', () => {
     console.log('running a task every minute');
     //getLatestNav();
-  }, { timezone: "Asia/Kolkata" });
+  });
   return {
     statusCode: 200,
     body: "Hello, World "
